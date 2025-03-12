7be458928d55c6010258144e669607d6a54cf5c4
lamyea-salma016 <lamyeasalma147@gmail.com>
2025-03-12 10:21:07 +0600

    hi

---
+++ lib/utils/constants/color.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/physics.dart';
+
+class Fcolors {
+  //primary
+  static const primary = Color(0xFF5CC936);
+  static const primaryDark = Color(0xFFF5C120);
+  static const primaryLight = Color(0xFFE5E5E5);
